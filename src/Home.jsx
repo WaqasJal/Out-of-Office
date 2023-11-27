37e3f691f1be93374487a55db50ffa08a75481b9
ammarsyed143 <77542791+ammarsyed143@users.noreply.github.com>
2023-11-26 22:08:37 -0500

    home

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,23 +2,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../src/images/OOOLogo.svg';
+import backgroundImages from '../src/images/Work.png'
+import RotatingBackground from './RotatingBackground';
 
 const Home = () => {
-    console.log('Home component rendered');  
+  const backgroundImages = [
+    '/images/Work.png',
+    '/images/Services.png',
+    '/images/Approach.png',
+  ];
+
   return (
-    <div>
-      <img src={logo} alt="Company Logo" className="logo" />
-      <h1>Welcome to the Home Page</h1>
-      <nav>
-        <Link to="/work">work</Link>
-        <Link to="/services">services</Link>
-        <Link to="/approach">approach</Link>
-      </nav>
-    </div>
+    <RotatingBackground backgroundImages={backgroundImages}>
+      <div>
+        <img src={logo} alt="Company Logo" className="logo" />
+        <h1>Welcome to the Home Page</h1>
+        <nav>
+          <Link to="/work">work</Link>
+          <Link to="/services">services</Link>
+          <Link to="/approach">approach</Link>
+        </nav>
+      </div>
+    </RotatingBackground>
   );
 };
-
-
-
 
 export default Home;
---
+++ src/RotatingBackground.jsx
@@ -0,0 +1,29 @@
+// RotatingBackground.js
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const RotatingBackground = ({ backgroundImages, children }) => {
+  const [rotation, setRotation] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const rotateBackground = () => {
+    setRotation(rotation + 90);
+    setCurrentImage((prev) => (prev + 1) % backgroundImages.length);
+  };
+
+  return (
+    <motion.div
+      className="rotating-background"
+      onClick={rotateBackground}
+      style={{
+        rotate: `${rotation}deg`,
+        backgroundImage: `url(${process.env.PUBLIC_URL}${backgroundImages[currentImage]})`,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default RotatingBackground;
+
