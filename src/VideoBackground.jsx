05e99251c85221cff82375a178342839fba4981b
Waqas Jalali <jwaqas2@gmail.com>
2023-12-13 00:31:29 -0500

    logo rotation

--- src/VideoBackground.jsx
+++ src/VideoBackground.jsx
@@ -1,52 +1,48 @@
-// Approach.js
-import React, {useState, Text, StyleSheet} from 'react';
+import React, { useState, Text, StyleSheet } from "react";
 
 //import './ApproachStyle.css'; // Import the CSS file
 //import tailwind from 'tailwind';
-import VideoApproach3 from '../src/videos/VideoApproach3.mp4'
-import VideoApproach2 from '../src/videos/VideoApproach2.mp4'
+import { motion } from "framer-motion";
+import VideoApproach3 from "../src/videos/VideoApproach3.mp4";
+import VideoApproach2 from "../src/videos/VideoApproach2.mp4";
+import { Link } from "react-router-dom";
+import logo from "../src/images/OOOLogo.svg";
 
-function VideoBackground  ({title,description})  {
+function VideoBackground({ title, description }) {
   return (
     <div className="relative w-full h-screen">
-
-      <video className="absolute w-full h-screen inset-0 object-cover opacity-90" autoPlay loop muted>
+      <video
+        className="absolute w-full h-screen inset-0 object-cover opacity-90"
+        autoPlay
+        loop
+        muted
+      >
         <source src={VideoApproach3} type="video/mp4" />
       </video>
-
-      <div className="absolute flex flex-col justify-center-left items-center-left top-48 left-8">
-
-        <h1 className="text-white font-bold text-2xl mt-20 mb-4 border-2 border-indigo-500/100 px-4 py-2 w-[567px] text-white text-[25px] font-bold font-['Inter'] uppercase tracking-[11.21px]">
-          {title}
-        </h1>
-
-        <p className="text-white text-opacity-75 text-4xl border-2 border-indigo-500/100 px-4 py-2 w-[567px] text-white text-6xl font-thin font-['Inter']">
-          {description}
-        </p>
-
+      <div>
+        <div className="p-4">
+          <Link to="/">
+            <div className=" flex items-center w-16 h-16">
+              <motion.div
+                animate={{ rotate: 360 }}
+                transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+              >
+                <img src={logo} alt="Company Logo" className="logo" />
+              </motion.div>
+            </div>
+          </Link>
+        </div>
+        <div className="absolute flex flex-col justify-center-left items-center-left top-48 left-8">
+          <h1 className="text-white font-bold text-2xl mt-20 mb-4  px-4 py-2 w-[567px] text-white text-[19px] font-bold font-['Inter'] uppercase tracking-[11.21px]">
+            {title}
+          </h1>
+          <p className="text-white text-opacity-75 text-4xl  px-4 py-2 w-[567px] text-white text-5xl font-thin font-['Inter']">
+            {description}
+          </p>
+        </div>
       </div>
-
-      {/* <div className="absolute inset-x-0 bottom-0 ">
-
-        <video className="absolute object-cover opacity-75" autoPlay loop muted>
-          <source src={VideoApproach3} type="video/mp4" />
-        </video>
-        
-      </div> */}
-
-
-      {/* <div className="absolute inset-x-0 bottom-1 ">
-
-        <video className="absolute object-cover opacity-75" autoPlay loop muted>
-          <source src={VideoApproach3} type="video/mp4" />
-        </video>
-
-      </div> */}
-      
     </div>
-
   );
-
 }
 
 export default VideoBackground;
