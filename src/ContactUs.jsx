03b83a7db387038a0cb6700aa1d4c51fcd4b9cae
ammarsyed143 <77542791+ammarsyed143@users.noreply.github.com>
2023-12-19 02:18:45 -0500

    done

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,111 +1,159 @@
 import React, { useState } from "react";
 import { Frame } from "../src/Frame"; // Update this import based on your component structure
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import logo from "../src/images/OOOLogo.svg";
 
 export const ContactUs = () => {
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        message: "",
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your form submission logic here
+    console.log("Form submitted:", formData);
+    // Reset form data after submission if needed
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      message: "",
     });
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Add your form submission logic here
-        console.log("Form submitted:", formData);
-        // Reset form data after submission if needed
-        setFormData({
-            firstName: "",
-            lastName: "",
-            email: "",
-            message: "",
-        });
-    };
-
-    return (
-        <div className="bg-[#0d1418] flex flex-row justify-center w-full">
-            <div className="bg-[#0d1418] w-[1920px] h-[1080px] relative">
-                <p className="absolute w-[693px] top-[232px] left-[56px] text-white text-5xl font-thin font-Inter">
-                    Gorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <img
-                    className="absolute w-[80px] h-[80px] top-[74px] left-[46px]"
-                    alt="Ooo"
-                    src="https://i.gyazo.com/d01ba3e1a57ff268e8d0539bb981df8e.png" // Replace with your actual image URL
-                />
-                <div className="flex flex-wrap w-[804px] items-start justify-end gap-[0px_0px] absolute top-[207px] left-[1058px]">
-                    <div className="w-[402px] relative">
-                        <Frame />
-                        <input
-                            type="text"
-                            name="firstName"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                            placeholder="First Name"
-                            className="border-none border-b border-white text-white bg-transparent focus:border-none focus:outline-none focus:ring-0 absolute left-0 right-0 top-[80px]"
-                        />
-                    </div>
-                    <div className="w-[402px] relative">
-                        <Frame text="L A S T    N A M E" />
-                        <input
-                            type="text"
-                            name="lastName"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                            placeholder="Last Name"
-                            className="border-none border-b border-white text-white bg-transparent focus:border-none focus:outline-none focus:ring-0 absolute left-0 right-0 top-[80px]"
-                        />
-                    </div>
-                    <div className="w-[804px] relative">
-                        <Frame text="E M A I L" />
-                        <input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            placeholder="Email"
-                            className="border-none border-b border-white text-white bg-transparent focus:border-none focus:outline-none focus:ring-0 absolute left-0 right-0 top-[80px]"
-                        />
-                    </div>
-                    <div className="w-[804px] relative">
-    <Frame text="M E S S A G E" />
-    <textarea
-        name="message"
-        value={formData.message}
-        onChange={handleChange}
-        placeholder="Message"
-        rows="5"
-        className="border-none border-b border-white text-white bg-transparent focus:border-none focus:outline-none focus:ring-0 absolute left-0 right-0 top-[80px] w-full"
-        style={{ width: '100%' }}
-    />
-</div>
-                </div>
-                <form onSubmit={handleSubmit} className="absolute top-[791px] left-[1699px]">
-                    <button
-                        style={{
-                            color: 'white',
-                            fontSize: '0.75rem', // equivalent to text-xs
-                            fontWeight: 'bold',
-                            textTransform: 'uppercase',
-                            fontFamily: 'Inter, sans-serif',
-                            letterSpacing: '0.177em', // equivalent to tracking-[7.08px]
-                        }}
-                        type="submit"
+  return (
+    <div>
+<div className="">
+        <Link to="/">
+          <div className=" flex items-center w-16 h-16 fixed z-10 mt-4 ml-4">
+            <motion.div
+              animate={{ rotate: 360 }}
+              transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+            >
+              <img src={logo} alt="Company Logo" className="logo" />
+            </motion.div>
+          </div>
+        </Link>
+      </div>
+    <div className="bg-[#0d1418] w-full h-screen relative flex justify-end">
+        
+      <div className="flex items-center"> {/* Centering vertically */}
+      <p className=" absolute flex justify-center-left items-center-left top-80 left-24 w-1/4 text-white text-5xl font-thin font-Inter">
+        Gorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      
+      
+      <div>
+        <section className="text-gray-700 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-12"></div>
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+              <div className="flex flex-wrap -m-2">
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      style={{
+                        fontFamily: "Inter, sans-serif",
+                        fontWeight: "bold",
+                        fontSize: "0.75rem",
+                        letterSpacing: "0.177em",
+                        textTransform: "uppercase",
+                        color: "white",
+                      }}
+                      htmlFor="name"
+                      className="leading-7 text-sm text-gray-600"
                     >
-                        Submit
-                    </button>
-                </form>
-                <img
-                    className="!fixed !left-0 !top-[941px]"
-                    alt="Brand Name"
-                    src="https://i.gyazo.com/d01ba3e1a57ff268e8d0539bb981df8e.png" // Replace with your actual brand image URL
-                />
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="w-full border-b border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out bg-transparent"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      style={{
+                        fontFamily: "Inter, sans-serif",
+                        fontWeight: "bold",
+                        fontSize: "0.75rem",
+                        letterSpacing: "0.177em",
+                        textTransform: "uppercase",
+                        color: "white",
+                      }}
+                      htmlFor="email"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full border-b border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out bg-transparent"
+                    />
+
+                  </div>
+                </div>
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label
+                      style={{
+                        fontFamily: "Inter, sans-serif",
+                        fontWeight: "bold",
+                        fontSize: "0.75rem",
+                        letterSpacing: "0.177em",
+                        textTransform: "uppercase",
+                        color: "white",
+                      }}
+                      htmlFor="message"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="w-full h-32 border-b border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out bg-transparent"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="p-2 w-full flex justify-end"> {/* Positioned at the bottom right */}
+                <button
+                  className="text-white border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                  style={{
+                    fontFamily: "Inter, sans-serif",
+                    fontWeight: "bold",
+                    fontSize: "0.75rem",
+                    letterSpacing: "0.177em",
+                    textTransform: "uppercase",
+                  }}
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        </section>
+      </div>
+    </div>
+     </div>
+     </div>
+
+  );
 };
