8de2c636c1e605cf5d2e355ee1c9893744f660b2
Waqas Jalali <jwaqas2@gmail.com>
2023-12-20 00:01:41 -0500

    contact us page mobile fix

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,20 +1,26 @@
 // Navigation.js
-import React from 'react';
-import './Navigation.css';
+import React from "react";
+import "./Navigation.css";
 import { Link } from "react-router-dom";
-import Socials from './Socials';
+import Socials from "./Socials";
+
 const Navigation = () => {
   return (
     <nav className="top-nav">
       <ul>
-        <li><Link to={`/`}>{"HOME"}</Link></li>
-        <li><Link to={`/work`}>{"WORK"}</Link></li>
-        <li><Link to={`/services`}>{"SERVICES"}</Link></li>
-        <li><Link to={`/approach`}>{"APPROACH"}</Link></li>
-        
-        
+        <li>
+          <Link to={`/`}>{"HOME"}</Link>
+        </li>
+        <li>
+          <Link to={`/work`}>{"WORK"}</Link>
+        </li>
+        <li>
+          <Link to={`/services`}>{"SERVICES"}</Link>
+        </li>
+        <li>
+          <Link to={`/approach`}>{"APPROACH"}</Link>
+        </li>
       </ul>
-      
     </nav>
   );
 };
--- src/VideoBackground.jsx
+++ src/VideoBackground.jsx
@@ -18,56 +18,56 @@ function VideoBackground({ title, description, videoUrl }) {
   const instagramProfileUrl = "https://www.instagram.com/outofofficeoriginal/";
   return (
     <div className="bg-custom-background-color relative w-full h-screen navbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-500">
-    <video
-      className="absolute w-full h-screen inset-0 object-cover opacity-10"
-      autoPlay
-      loop
-      muted
-      playsInline
-    controls={false} // Set controls to false to hide the default video controls
-    >
-      <source src={VideoApproach3} type="video/mp4" />
-    </video>
-    <div className="p-4">
-      <Link to="/">
-        <div className="flex items-center w-16 h-16 fixed z-10">
-          <motion.div
-            animate={{ rotate: 360 }}
-            transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
-          >
-            <img src={logo} alt="Company Logo" className="logo" />
-          </motion.div>
-        </div>
-      </Link>
-    </div>
-    <div className="absolute top-1/2 transform -translate-y-1/2 left-8 text-left">
-        <h1 className="text-white font-bold text-2xl mt-[-300px] mb-2 md:mt-[-200px] md:mb-4 px-4 py-2 max-w-md md:max-w-2xl text-white text-3xl md:text-3xl lg:text-3xl font-bold font-['Inter'] uppercase tracking-[11.21px]">
+      <video
+        className="absolute w-full h-screen inset-0 object-cover opacity-10"
+        autoPlay
+        loop
+        muted
+        playsInline
+        controls={false} // Set controls to false to hide the default video controls
+      >
+        <source src={VideoApproach3} type="video/mp4" />
+      </video>
+      <div className="p-4">
+        <Link to="/">
+          <div className="flex items-center w-16 h-16 fixed z-10">
+            <motion.div
+              animate={{ rotate: 360 }}
+              transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+            >
+              <img src={logo} alt="Company Logo" className="logo" />
+            </motion.div>
+          </div>
+        </Link>
+      </div>
+      <div className="absolute top-1/2 transform -translate-y-1/2 left-8 text-left">
+        <h1 className="text-white font-bold text-2xl mt-[-300px] mb-2 md:mt-[-200px] md:mb-4 px-4 py-2 max-w-md md:max-w-2xl text-white text-3xl md:text-3xl lg:text-xl font-bold font-['Inter'] uppercase tracking-[11.21px]">
           {title}
         </h1>
         <p className="text-white text-opacity-75 text-4xl md:text-4xl lg:text-3xl px-4 py-2 max-w-md md:max-w-2xl text-white font-thin font-['Inter'] whitespace-normal">
           {description}
         </p>
       </div>
-        {/* Bottom left text */}
-        <div className="absolute bottom-0 left-0 p-4 inline-flex items-center justify-end gap-[18px] opacity-70">
-          <div className="relative w-fit  font-normal text-white text-24 tracking-0 leading-normal">
-            OUT OF OFFICE ™
-          </div>
+      {/* Bottom left text */}
+      <div className="fixed  bottom-0 left-0 p-4 inline-flex items-center justify-end gap-[18px] opacity-70">
+        <div className="relative w-fit  font-normal text-white text-24 tracking-0 leading-normal">
+          OUT OF OFFICE ™
         </div>
-        {/* Bottom right icons */}
-        <div className="fixed bottom-0 right-0 p-4 z-10">
-          <div className="inline-flex items-center justify-end gap-[18px] relative">
-            {[envelopeUrl].map((url, index) => (
-              <Link to="/contactus">
-                <img
-                  key={index}
-                  className="w-[24px] h-[24px]"
-                  alt={`Icon ${index + 1}`}
-                  src={url}
-                />
-              </Link>
-            ))}
-            {/* {[deviceMobileUrl].map((url, index) => (
+      </div>
+      {/* Bottom right icons */}
+      <div className="fixed bottom-0 right-0 p-4 z-10">
+        <div className="inline-flex items-center justify-end gap-[18px] relative">
+          {[envelopeUrl].map((url, index) => (
+            <Link to="/contactus">
+              <img
+                key={index}
+                className="w-[24px] h-[24px]"
+                alt={`Icon ${index + 1}`}
+                src={url}
+              />
+            </Link>
+          ))}
+          {/* {[deviceMobileUrl].map((url, index) => (
               <img
                 key={index}
                 className="w-[24px] h-[24px]"
@@ -75,25 +75,23 @@ function VideoBackground({ title, description, videoUrl }) {
                 src={url}
               />
             ))} */}
-            <a
-              href={instagramProfileUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {[instagramLogoUrl].map((url, index) => (
-                <img
-                  key={index}
-                  className="w-[24px] h-[24px]"
-                  alt={`Icon ${index + 1}`}
-                  src={url}
-                />
-              ))}
-            </a>
-          </div>
+          <a
+            href={instagramProfileUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {[instagramLogoUrl].map((url, index) => (
+              <img
+                key={index}
+                className="w-[24px] h-[24px]"
+                alt={`Icon ${index + 1}`}
+                src={url}
+              />
+            ))}
+          </a>
         </div>
       </div>
-
-    
+    </div>
   );
 }
 
