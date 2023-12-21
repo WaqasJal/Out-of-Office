d44ea4d7e0f5416203d575144b5ea5be629b3e87
ammarsyed143 <77542791+ammarsyed143@users.noreply.github.com>
2023-12-20 22:22:40 -0500

    final

--- src/VideoBackground.jsx
+++ src/VideoBackground.jsx
@@ -40,11 +40,11 @@ function VideoBackground({ title, description, videoUrl }) {
           </div>
         </Link>
       </div>
-      <div className="absolute top-1/2 transform  left-8 text-left mt-[-300px] md:mt-[-200px]">
-        <h1 className="py-4 text-white font-bold text-2xl mb-2 md:mb-4 px-1 md:py-16 max-w-md md:max-w-2xl text-white text-3xl md:text-3xl lg:text-xl font-bold font-['Inter'] uppercase tracking-[11.21px]">
+      <div className="absolute top-1/2 transform lg:mt-[-350px] left-8 text-left mt-[-300px] md:mt-[-200px] ">
+        <h1 className="py-4 text-white font-bold text-2xl mb-2 md:mb-4 px-1 md:py-16 max-w-md md:max-w-2xl text-white text-3xl md:text-2xl lg:text-xl font-bold font-['Inter'] uppercase tracking-[11.21px]">
           {title}
         </h1>
-        <div className="py-40 text-white text-opacity-75 text-4xl md:text-4xl lg:text-3xl px-1 py-8 md:py-8 max-w-md md:max-w-2xl text-white font-thin font-['Inter'] whitespace-normal">
+        <div className="lg:mt-[-50px] py-40 text-white text-opacity-75 text-4xl md:text-4xl lg:text-4xl px-1 py-8 md:py-8 max-w-md md:max-w-2xl text-white font-thin font-['Inter'] whitespace-normal">
           {description}
         </div>
       </div>
